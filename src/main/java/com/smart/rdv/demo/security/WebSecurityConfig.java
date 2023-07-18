10a8832e64313a38c78f8153394f598e3deb869d
ahmed787865 <ahmedbile126@gmail.com>
2023-07-18 13:57:39 +0000

    test API REST

--- src/main/java/com/smart/rdv/demo/security/WebSecurityConfig.java
+++ src/main/java/com/smart/rdv/demo/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -29,7 +30,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
   @Autowired
   UserDetailsServiceImpl userDetailsService;
 
-  @Autowired
+  @Autowired 
   private AuthEntryPointJwt unauthorizedHandler;
 
   @Bean
